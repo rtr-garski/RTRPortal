4c45ea543a201d127600463e407a5e3de90c858c
Garry <garry@Garski-MBPro-2018.local>
2026-04-07 07:50:27 +0800

    your changes

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <?php $title = "Projects Dashboard"; include('./partials/title-meta.php'); ?> <?php include('./partials/head-css.php'); ?>
+        <?php $title = "Projects Dashboard"; include('partials/title-meta.php'); ?> <?php include('partials/head-css.php'); ?>
     </head>
 
     <body>
         <!-- Begin page -->
         <div class="wrapper">
-            <?php include('./partials/topbar.php'); ?> <?php include('./partials/sidenav.php'); ?>
+            <?php include('partials/topbar.php'); ?> <?php include('partials/sidenav.php'); ?>
 
             <!-- ============================================================== -->
             <!-- Start Main Content -->
@@ -665,7 +665,7 @@
                 </div>
                 <!-- container -->
 
-                <?php include('./partials/footer.php'); ?>
+                <?php include('partials/footer.php'); ?>
             </div>
 
             <!-- ============================================================== -->
@@ -674,7 +674,7 @@
         </div>
         <!-- END wrapper -->
 
-        <?php include('./partials/customizer.php'); ?> <?php include('./partials/footer-scripts.php'); ?>
+        <?php include('partials/customizer.php'); ?> <?php include('partials/footer-scripts.php'); ?>
 
         <!-- E Charts js -->
         <script src="assets/plugins/echarts/echarts.min.js"></script>
