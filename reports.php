475f442132e4d75628ed313250745b871ff4f0e6
Garry <garry@Garski-MBPro-2018.local>
2026-04-10 04:49:40 +0800

    garski changed something

--- reports.php
+++ reports.php
@@ -118,7 +118,7 @@
 
 					<div class="row">
 						<div class="col-12">
-							<div data-table data-table-rows-per-page="8" class="card">
+							<div data-table data-table-rows-per-page="5" class="card">
 								<div class="card-header border-light justify-content-between">
 									<div class="d-flex gap-2">
 										<div class="app-search">
