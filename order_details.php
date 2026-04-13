3ec9cfbacf626975ec0ac2550a5fdc486f627a21
Garry <garry@Garski-MBPro-2018.local>
2026-04-14 07:17:13 +0800

    garski changed something

--- order_details.php
+++ order_details.php
@@ -177,7 +177,8 @@ $order['insurance'] = $insurance;
 															<input type="text" id="<?= rand(0,1) ? 'validInput' : 'inValidationInput' ?>" class="form-control mb-1 is-valid" value="<?= htmlspecialchars($loc['Loc_Name']) ?>">
 														<p class="mb-0"><i class="ti ti-map-pin me-1"></i><?= htmlspecialchars($loc['Loc_Address_Street']) ?>, <?= htmlspecialchars($loc['Loc_Address_City']) ?>, <?= htmlspecialchars($loc['Loc_Address_State']) ?> <?= htmlspecialchars($loc['Loc_Address_Zip']) ?></p>
 														<p class="mb-0 text-muted"><i class="ti ti-phone me-1"></i><?= htmlspecialchars($loc['Loc_Address_Phone']) ?> &nbsp; <i class="ti ti-printer me-1"></i><?= htmlspecialchars($loc['Loc_Address_Phone_Fax']) ?></p>
-														<div class="invalid-feedback">Need to Update</div>
+														<div class="valid-feedback">Correct Info</div>
+														<div class="invalid-feedback">Please Select Correct Location</div>
 													</div>
 													<div class="col-md-10">
 														<label class="form-label fw-semibold">Special Instructions</label>
