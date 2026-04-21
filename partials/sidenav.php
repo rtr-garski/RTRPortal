c8d7016fc44429eb7973c709322960c39c92e7c8
Garry <garry@Garski-MBPro-2018.local>
2026-04-22 00:25:07 +0800

    garski changed something

--- partials/sidenav.php
+++ partials/sidenav.php
@@ -1,6 +1,6 @@
 <div class="sidenav-menu">
     <!-- Brand Logo -->
-    <a href="index.php" class="logo">
+    <!-- <a href="index.php" class="logo">
         <span class="logo logo-light">
             <span class="logo-lg"><img src="../source/inspinia5/assets/images/logo.png" alt="logo" /></span>
             <span class="logo-sm"><img src="../source/inspinia5/assets/images/logo-sm.png" alt="small logo" /></span>
@@ -10,7 +10,7 @@
             <span class="logo-lg"><img src="../source/inspinia5/assets/images/logo-black.png" alt="dark logo" /></span>
             <span class="logo-sm"><img src="../source/inspinia5/assets/images/logo-sm.png" alt="small logo" /></span>
         </span>
-    </a>
+    </a> -->
 
     <!-- Sidebar Hover Menu Toggle Button -->
     <button class="button-on-hover">
@@ -23,54 +23,6 @@
     </button>
 
     <div class="scrollbar" data-simplebar="">
-        <div id="user-profile-settings" class="sidenav-user" style="background: url(../source/inspinia5/assets/images/user-bg-pattern.svg)">
-            <div class="d-flex justify-content-between align-items-center">
-                <div>
-                    <a href="#!" class="link-reset">
-                        <img src="../source/inspinia5/assets/images/users/user-1.jpg" alt="user-image" class="rounded-circle mb-2 avatar-md" />
-                        <span class="sidenav-user-name fw-bold">John Smith</span>
-                        <span class="fs-12 fw-semibold" data-lang="user-role">Verifier</span>
-                    </a>
-                </div>
-                <div>
-                    <a class="dropdown-toggle drop-arrow-none link-reset sidenav-user-set-icon" data-bs-toggle="dropdown" data-bs-offset="0,12" href="#!" aria-haspopup="false" aria-expanded="false">
-                        <i class="ti ti-settings fs-24 align-middle ms-1"></i>
-                    </a>
-
-                    <div class="dropdown-menu">
-                        <!-- Header -->
-                        <div class="dropdown-header noti-title">
-                            <h6 class="text-overflow m-0">Welcome back!</h6>
-                        </div>
-
-                        <!-- My Profile -->
-                        <a href="#!" class="dropdown-item">
-                            <i class="ti ti-user-circle me-1 fs-lg align-middle"></i>
-                            <span class="align-middle">Profile</span>
-                        </a>
-
-                        <!-- Settings -->
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <i class="ti ti-settings-2 me-1 fs-lg align-middle"></i>
-                            <span class="align-middle">Account Settings</span>
-                        </a>
-
-                        <!-- Lock -->
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <i class="ti ti-lock me-1 fs-lg align-middle"></i>
-                            <span class="align-middle">Lock Screen</span>
-                        </a>
-
-                        <!-- Logout -->
-                        <a href="javascript:void(0);" class="dropdown-item text-danger fw-semibold">
-                            <i class="ti ti-logout me-1 fs-lg align-middle"></i>
-                            <span class="align-middle">Log Out</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!--- Sidenav Menu -->
         <div id="sidenav-menu">
             <ul class="side-nav">
