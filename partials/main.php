8639e12a4e8ab82dad9052013e1881e0bde4bbbd
Garry <garry@Garski-MBPro-2018.local>
2026-04-22 07:32:18 +0800

    garski changed something

--- partials/main.php
+++ partials/main.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../config/session.php';
 
 if (empty($_SESSION['user_id'])) {
-    header('Location: /login.php');
+    $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+    header('Location: ' . $base . '/login.php');
     exit;
 }
