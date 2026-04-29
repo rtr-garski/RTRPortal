4d03b375485c29b8fef9dfbca05c961c5cc690b7
Garry <garry@Garski-MBPro-2018.local>
2026-04-30 00:17:15 +0800

    Update

--- config/login_log.php
+++ config/login_log.php
@@ -3,11 +3,7 @@ function get_client_ip(): string {
     foreach (['HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'REMOTE_ADDR'] as $key) {
         if (!empty($_SERVER[$key])) {
             $ip = trim(explode(',', $_SERVER[$key])[0]);
-            // Convert IPv6-mapped IPv4 (::ffff:1.2.3.4) to plain IPv4
-            if (str_starts_with($ip, '::ffff:')) {
-                $ip = substr($ip, 7);
-            }
-            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) return $ip;
+            if (filter_var($ip, FILTER_VALIDATE_IP)) return $ip;
         }
     }
     return '0.0.0.0';
