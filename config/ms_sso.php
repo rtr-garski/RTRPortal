3a49a41f7d6cae98323ca5819dacd640ab197ce7
Garry <garry@Garski-MBPro-2018.local>
2026-04-29 22:46:12 +0800

    Update

--- config/ms_sso.php
+++ config/ms_sso.php
@@ -1,5 +1,5 @@
 <?php
-define('MS_CLIENT_ID',     'YOUR_CLIENT_ID');
-define('MS_CLIENT_SECRET', 'YOUR_CLIENT_SECRET');
-define('MS_TENANT_ID',     'YOUR_TENANT_ID');
+define('MS_CLIENT_ID',     'bbd3f148-fdc9-41c2-b0da-fa836a663691');
+define('MS_CLIENT_SECRET', '7093a6e5-755e-4208-83d4-db3db90c9ad8');
+define('MS_TENANT_ID',     '130ba21c-f46e-48bb-8310-abe42d7ced8a');
 define('MS_REDIRECT_URI',  'https://yourdomain.com/auth/ms_callback.php');
